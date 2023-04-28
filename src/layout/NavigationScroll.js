00b9ce9ae79ba0a72aefab03ddb78d83ebdaa4db
wjourney <1061761214@qq.com>
2023-04-28 16:53:46 +0800

    fix: remove mockdata

--- src/layout/NavigationScroll.js
+++ src/layout/NavigationScroll.js
@@ -3,8 +3,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // ==============================|| NAVIGATION SCROLL TO TOP ||============================== //
-// export const isDev2 = typeof window !== 'undefined' && window.location.hostname?.split('.')[0] === 'dev2'
-export const isDev2 = true
+export const isDev2 = typeof window !== 'undefined' && window.location.hostname?.split('.')[0] === 'dev2'
 export const remagiImg = "https://bkmk.oss-accelerate.aliyuncs.com/41327180-e58e-11ed-971e-c60e34317deb?OSSAccessKeyId=LTAI5tPynodLHeacT1J5SmWh&Expires=317042663466&Signature=I6hSw9yCQRxTdFmf3claU4oFxU4%3D"
 export const miaohuaImg =
     'https://bkmk.oss-accelerate.aliyuncs.com/icon%2Fmiaohua.png?OSSAccessKeyId=LTAI5tPynodLHeacT1J5SmWh&Expires=317040945755&Signature=mcp38UBWAZ9%2BoK4AjiCBGL01hwY%3D';
