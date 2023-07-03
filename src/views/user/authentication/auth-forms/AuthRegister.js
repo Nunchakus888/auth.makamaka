23165c71f592fe7ca2da565bfafe2529a5699430
huopanpan <huopanpan@sensetime.com>
2023-07-03 17:56:53 +0800

    用户隐私协议

--- src/views/user/authentication/auth-forms/AuthRegister.js
+++ src/views/user/authentication/auth-forms/AuthRegister.js
@@ -30,7 +30,7 @@ import {
   Typography,
   useMediaQuery
 } from '@mui/material';
-import {Trans, useTranslation} from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 // third party
 import * as Yup from 'yup';
@@ -51,8 +51,8 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import * as Api from "../../../../api";
 import history from "../../../../routes/history";
 import useToast from "../../../../hooks/useToast";
-import {THIRDPARTYLOGINURL} from "../../../../utils/constant/signupConstant";
-import {styled} from "@mui/styles";
+import { THIRDPARTYLOGINURL } from "../../../../utils/constant/signupConstant";
+import { styled } from "@mui/styles";
 import PropTypes from "prop-types";
 
 // ===========================|| FIREBASE - REGISTER ||=========================== //
@@ -105,22 +105,28 @@ const FirebaseRegister = ({ ...others }) => {
   const [showPassword, setShowPassword] = useState(false);
   const [checked, setChecked] = useState(true);
   const toast = useToast();
-  const [privide, setProvide] = useState(null);
-  const [privideuserid, setProvideuserid] = useState('');
-  const [privideuserlogin, setProvidesuerlogin] = useState('');
   const [logintype, setLogintype] = useState(0);
-  const [open, setOpen] = useState(false);
+  const [userAgreement, setUserAgreement] = useState(false);
+  const [privacyPolicy, setPrivacyPolicy] = useState(false);
   const urlSearch = new URLSearchParams(history.location.search);
   const [invitecode, setInvitecode] = useState(urlSearch.get('invitecode'));
 
   const [strength, setStrength] = useState(0);
   const [level, setLevel] = useState();
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleClickUserAgreementOpen = () => {
+    // setUserAgreement(true);
+    window.open('./user_agreement.html');
   };
-  const handleClose = () => {
-    setOpen(false);
+  const handleClickPrivacyPolicyOpen = () => {
+    // setPrivacyPolicy(true);
+    window.open('./privacy_policy.html');
+  };
+  const handleUserAgreementClose = () => {
+    setUserAgreement(false);
+  };
+  const handlePrivacyPolicyClose = () => {
+    setPrivacyPolicy(false);
   };
 
   const googleHandler = () => {
@@ -150,7 +156,7 @@ const FirebaseRegister = ({ ...others }) => {
     //监测URL中是否包含其他信息
     const urls = new URLSearchParams(history.location.search);
     setLogintype(urls.get('login'));
-    if(urls.get('login') === '1'){
+    if (urls.get('login') === '1') {
       // location.replace('/paint');
       location.replace(jumpLink);
     }
@@ -163,43 +169,43 @@ const FirebaseRegister = ({ ...others }) => {
           !isMiaohua && (
             <Fragment>
               <Grid item xs={12}>
-          <AnimateButton>
-            <Button
-              variant="outlined"
-              fullWidth
-              onClick={googleHandler}
-              size="large"
-              sx={{
-                color: 'grey.700',
-                backgroundColor: theme.palette.grey[50],
-                borderColor: theme.palette.grey[100]
-              }}
-            >
-              <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
-                <img src={Google} alt="google" width={16} height={16} style={{ marginRight: matchDownSM ? 8 : 16 }} />
-              </Box>
-              <Trans i18nKey="user.register_google">{ defaultLanguage.user.register_google }</Trans>
-            </Button>
-          </AnimateButton>
-          <AnimateButton>
-            <Button
-              variant="outlined"
-              fullWidth
-              onClick={discordHandler}
-              size="large"
-              sx={{
-                color: 'grey.700',
-                backgroundColor: theme.palette.grey[50],
-                borderColor: theme.palette.grey[100],
-                mt:2
-              }}
-            >
-              <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
-                <img src={"https://bkmk.oss-accelerate.aliyuncs.com/discord-icon.png?OSSAccessKeyId=LTAI5tPynodLHeacT1J5SmWh&Expires=317031892813&Signature=HWVUkGjrF38Dt8qmSTPBJ2bBAdI%3D"} alt="discord" width={16} height={16} style={{ marginRight: matchDownSM ? 8 : 16 }} />
-              </Box>
-              <Trans i18nKey="user.register_discord">{ defaultLanguage.user.register_discord }</Trans>
-            </Button>
-          </AnimateButton>
+                <AnimateButton>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={googleHandler}
+                    size="large"
+                    sx={{
+                      color: 'grey.700',
+                      backgroundColor: theme.palette.grey[50],
+                      borderColor: theme.palette.grey[100]
+                    }}
+                  >
+                    <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
+                      <img src={Google} alt="google" width={16} height={16} style={{ marginRight: matchDownSM ? 8 : 16 }} />
+                    </Box>
+                    <Trans i18nKey="user.register_google">{defaultLanguage.user.register_google}</Trans>
+                  </Button>
+                </AnimateButton>
+                <AnimateButton>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={discordHandler}
+                    size="large"
+                    sx={{
+                      color: 'grey.700',
+                      backgroundColor: theme.palette.grey[50],
+                      borderColor: theme.palette.grey[100],
+                      mt: 2
+                    }}
+                  >
+                    <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
+                      <img src={"https://bkmk.oss-accelerate.aliyuncs.com/discord-icon.png?OSSAccessKeyId=LTAI5tPynodLHeacT1J5SmWh&Expires=317031892813&Signature=HWVUkGjrF38Dt8qmSTPBJ2bBAdI%3D"} alt="discord" width={16} height={16} style={{ marginRight: matchDownSM ? 8 : 16 }} />
+                    </Box>
+                    <Trans i18nKey="user.register_discord">{defaultLanguage.user.register_discord}</Trans>
+                  </Button>
+                </AnimateButton>
               </Grid>
               <Grid item xs={12}>
                 <Box sx={{ alignItems: 'center', display: 'flex' }}>
@@ -219,7 +225,7 @@ const FirebaseRegister = ({ ...others }) => {
                     disableRipple
                     disabled
                   >
-                    <Trans i18nKey="user.register_or">{ defaultLanguage.user.register_or }</Trans>
+                    <Trans i18nKey="user.register_or">{defaultLanguage.user.register_or}</Trans>
                   </Button>
                   <Divider sx={{ flexGrow: 1 }} orientation="horizontal" />
                 </Box>
@@ -230,28 +236,48 @@ const FirebaseRegister = ({ ...others }) => {
         <Grid item xs={12} container alignItems="center" justifyContent="center">
           <Box sx={{ mb: 2 }}>
             <Typography variant="subtitle1">
-              <Trans i18nKey="user.register_tips2">{ defaultLanguage.user.register_tips2 }</Trans>
+              <Trans i18nKey="user.register_tips2">{defaultLanguage.user.register_tips2}</Trans>
             </Typography>
           </Box>
         </Grid>
       </Grid>
 
       <BootstrapDialog
-        onClose={handleClose}
+        onClose={handleUserAgreementClose}
         aria-labelledby="customized-dialog-title"
-        open={open}
+        open={userAgreement}
       >
-        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-          <Trans i18nKey="user.register_agree_title">{ defaultLanguage.user.register_agree_title }</Trans>
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleUserAgreementClose}>
+          <Trans i18nKey="user.register_agree_title">{defaultLanguage.user.register_agree_title}</Trans>
         </BootstrapDialogTitle>
         <DialogContent dividers>
           <Typography gutterBottom>
-            <Trans i18nKey="user.register_agree_Content">{ defaultLanguage.user.register_agree_Content }</Trans>
+          <Trans i18nKey="user.register_agree_Content">{ defaultLanguage.user.register_agree_Content }</Trans>
           </Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>
-            <Trans i18nKey="user.register_agree_bottom">{ defaultLanguage.user.register_agree_bottom }</Trans>
+          <Button onClick={handleUserAgreementClose}>
+            <Trans i18nKey="user.register_agree_bottom">{defaultLanguage.user.register_agree_bottom}</Trans>
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+
+      <BootstrapDialog
+        onClose={handlePrivacyPolicyClose}
+        aria-labelledby="customized-dialog-title"
+        open={privacyPolicy}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handlePrivacyPolicyClose}>
+          <Trans i18nKey="user.register_agree_title">{defaultLanguage.user.register_agree_title}</Trans>
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+          <Trans i18nKey="user.register_agree_Content">{ defaultLanguage.user.register_agree_Content }</Trans>
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handlePrivacyPolicyClose}>
+            <Trans i18nKey="user.register_agree_bottom">{defaultLanguage.user.register_agree_bottom}</Trans>
           </Button>
         </DialogActions>
       </BootstrapDialog>
@@ -261,7 +287,7 @@ const FirebaseRegister = ({ ...others }) => {
           email: '',
           password: '',
           submit: null,
-          username:'',
+          username: '',
           invite_code: invitecode
         }}
         validationSchema={Yup.object().shape({
@@ -270,7 +296,7 @@ const FirebaseRegister = ({ ...others }) => {
           username: Yup.string().max(255).required('Username is required')
         })}
         onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-          if(logintype === '0'){
+          if (logintype === '0') {
             const urls = new URLSearchParams(history.location.search);
             // 加三个参数到value中
             values['provider'] = urls.get('provider');
@@ -280,19 +306,19 @@ const FirebaseRegister = ({ ...others }) => {
           }
           const { code, msg } = await Api.signup(values).catch(e => e);
           try {
-            if (code === 0){
+            if (code === 0) {
               //跳转到邮箱验证界面
               history.replace({ pathname: "/checking", state: {} });
               history.go(0)
 
-            }else if(code === 109){
+            } else if (code === 109) {
               //失败
               toast("邮箱已被注册" || Api.ERROR_MESSAGE, { variant: 'error' });
-            }else if(code === 110){
+            } else if (code === 110) {
               toast("用户名已被使用" || Api.ERROR_MESSAGE, { variant: 'error' });
-            }else if(code === 111){
+            } else if (code === 111) {
               toast("邮箱不通，发送邮件失败" || Api.ERROR_MESSAGE, { variant: 'error' });
-            }else if(code === 112){
+            } else if (code === 112) {
               toast("邀请码不对" || Api.ERROR_MESSAGE, { variant: 'error' });
             } else {
               toast(msg || Api.ERROR_MESSAGE, { variant: 'error' });
@@ -315,7 +341,7 @@ const FirebaseRegister = ({ ...others }) => {
           <form noValidate onSubmit={handleSubmit} {...others}>
             <FormControl fullWidth error={Boolean(touched.username && errors.username)} sx={{ ...theme.typography.customInput }}>
               <InputLabel htmlFor="outlined-adornment-email-register">
-                <Trans i18nKey="user.register_username">{ defaultLanguage.user.register_username }</Trans>
+                <Trans i18nKey="user.register_username">{defaultLanguage.user.register_username}</Trans>
               </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-email-register"
@@ -334,7 +360,7 @@ const FirebaseRegister = ({ ...others }) => {
             </FormControl>
             <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
               <InputLabel htmlFor="outlined-adornment-email-register">
-                <Trans i18nKey="user.register_email">{ defaultLanguage.user.register_email }</Trans>
+                <Trans i18nKey="user.register_email">{defaultLanguage.user.register_email}</Trans>
               </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-email-register"
@@ -354,7 +380,7 @@ const FirebaseRegister = ({ ...others }) => {
 
             <FormControl fullWidth error={Boolean(touched.password && errors.password)} sx={{ ...theme.typography.customInput }}>
               <InputLabel htmlFor="outlined-adornment-password-register">
-                <Trans i18nKey="user.register_password">{ defaultLanguage.user.register_password }</Trans>
+                <Trans i18nKey="user.register_password">{defaultLanguage.user.register_password}</Trans>
               </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-password-register"
@@ -434,9 +460,13 @@ const FirebaseRegister = ({ ...others }) => {
                   }
                   label={
                     <Typography variant="subtitle1">
-                      <Trans i18nKey="user.register_aw1">{ defaultLanguage.user.register_aw1 }</Trans>
-                      <Typography variant="subtitle1" component={Link} to="#" onClick={handleClickOpen}>
-                        <Trans i18nKey="user.register_aw2">{ defaultLanguage.user.register_aw2 }</Trans>
+                      <Trans i18nKey="user.register_aw1">{defaultLanguage.user.register_aw1}</Trans>
+                      <Typography variant="subtitle1" component={Link} to="#" onClick={handleClickUserAgreementOpen}>
+                        <Trans i18nKey="user.register_aw2.user_agreement">{defaultLanguage.user.register_aw2.user_agreement}</Trans>
+                      </Typography>
+                      <Trans i18nKey="user.register_aw2.and">{defaultLanguage.user.register_aw2.and}</Trans>
+                      <Typography variant="subtitle1" component={Link} to="#" onClick={handleClickPrivacyPolicyOpen}>
+                        <Trans i18nKey="user.register_aw2.privacy_policy">{defaultLanguage.user.register_aw2.privacy_policy}</Trans>
                       </Typography>
                     </Typography>
                   }
@@ -452,7 +482,7 @@ const FirebaseRegister = ({ ...others }) => {
             <Box sx={{ mt: 2 }}>
               <AnimateButton>
                 <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="secondary">
-                  <Trans i18nKey="user.register_lable">{ defaultLanguage.user.register_lable }</Trans>
+                  <Trans i18nKey="user.register_lable">{defaultLanguage.user.register_lable}</Trans>
                 </Button>
               </AnimateButton>
             </Box>
